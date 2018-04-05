50b96ea46915aea5a04b2903d4151255dd1d8693
JinFeng <907237587@qq.COM>
2018-04-05 12:35:20 +0800

    完成多文件上传

--- src/main/java/controller/FileUploadController.java
+++ src/main/java/controller/FileUploadController.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import bean.FileDomain;
+import domain.MultiFileDomain;
 
 /**
  * 
@@ -57,6 +60,26 @@ public class FileUploadController {
 		} catch (IOException e) {
 			// 
 			e.printStackTrace();
+		}
+	}
+	@RequestMapping("ajax/multifile")
+	public void multifileupload(MultiFileDomain multiFileDoamin, HttpServletRequest request,HttpServletResponse response){
+		String realPath = request.getServletContext().getRealPath("uploadfiles");
+		File targetDir = new File(realPath);
+		if(!targetDir.exists()){
+			targetDir.mkdir();
+		}
+		List<MultipartFile> files = (List<MultipartFile>) multiFileDoamin.getFiles();
+		for(int i=0;i<files.size();i++){
+			MultipartFile  file = files.get(i);
+			String filename = file.getOriginalFilename();
+			File targetFile = new File(realPath,filename);
+			try{
+				file.transferTo(targetFile);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			logger.info("文件上传成功");
 		}
 	}
 
