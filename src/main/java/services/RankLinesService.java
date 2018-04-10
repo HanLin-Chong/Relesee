1e182dd479e9599b8c738916c26dae72ce9dc147
JinFeng <907237587@qq.COM>
2018-04-10 22:09:10 +0800

    新增更具审核状态查找审核项目的结果集

--- src/main/java/services/RankLinesService.java
+++ src/main/java/services/RankLinesService.java
@@ -1,12 +1,27 @@
 package services;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import bean.RankItem;
 import dao.impl.RankItemDaoImpl;
 
 @Service("rankLinesService")
 public class RankLinesService {
 	@Autowired
 	private RankItemDaoImpl rankItemDao;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public List<RankItem> getRankItems(String status){
+		if("".equals(status) || status==null) status = "待审核";
+		this.rankItemDao.setJdbcTemplate(jdbcTemplate);
+		String sql = "select * from ranklines where states="+"'"+status+"'";
+		return this.rankItemDao.find(sql, RankItem.class);
+	}
 }
