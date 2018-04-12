4c296f60ec26230749484857ab343f93624dc757
JinFeng <907237587@qq.COM>
2018-04-12 20:56:07 +0800

    查询排队数据的借口

---
+++ src/main/java/websocket/MyWebSocketConfig.java
@@ -0,0 +1,24 @@
+package websocket;
+
+import javax.annotation.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Component
+@EnableWebMvc
+@EnableWebSocket
+public class MyWebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer{
+
+		@Resource
+		MyWebSocketHandler handler;
+		@Override
+		public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+			System.out.println("注册WebSocet");
+		     registry.addHandler(handler, "/wsMy").addInterceptors(new HandShake());  
+		     registry.addHandler(handler, "/wsMy/sockjs").addInterceptors(new HandShake()).withSockJS();  
+		}
+}
